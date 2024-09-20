db81cfefa2619071d028a77f41521b792ede28ea
DiegolMarques <thediegomarques@gmail.com>
2024-09-20 17:15:25 -0400

    home change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-center text-6xl font-bold">CHEM</h1>
+      <h1 className="text-center text-6xl font-bold">Home</h1>
     </div>
   );
 };
